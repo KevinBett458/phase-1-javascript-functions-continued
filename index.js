a5ff0e04c58c211867dee31083a134f22bc93dbe
KevinBett <kevinbett458@gmail.com>
2023-03-14 21:30:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate'){
+  return `This Saturday, I want to ${defaultActivity}!`
+}
+
+function mondayWork(defaultActivity = 'go to the office'){
+  return `This Monday, I will ${defaultActivity}.`
+}
+
+function wrapAdjective(symbol = '*'){
+  return function(adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  }
+}
